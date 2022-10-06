589fe9571de1e7d094148e0de5734f8ff5ed3eb7
Lulu Jovovich <lulujovovich@Lulus-MBP.lan>
2022-10-05 22:00:52 -0700

    updated readme, new images, final route edits

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # e-commerce-backend
+
+## Description
+
+The purpose of this project was to present the back end of an e-commerce website which would enable a user to read, create, update, and delete items in the store's database. After being given starter code, I wanted to enhance the database functionality via API routes so that the user would be able to keep their information organized. I did this through the use of Express, MySQL, and Sequelize. With the help of these technologies (and Insomnia for testing), a user is able to do the following:
+
+1. Read data (all or by ID) by searching /api/categories, /api/tags, and /api/products.
+2. Create data through /api/categories, /api/tags, and /api/products.
+3. Update data through /api/categories, /api/tags, and /api/products.
+4. Delete data through /api/categories, /api/tags, and /api/products.
+
+I accomplished most of this by creating API routes (in the routes folder) for all of the look-up methods (categories, tags, products), and that was a big learning experience. I also accomplished this project through the use of Models by defining them and creating constraints and validations. I also reinforced my own knowledge of seeding a schema, creating connections, APIs, and using the dotenv package.
+
+## Technology Used:
+
+1. Javascript
+2. Express
+3. MySQL2
+4. Sequelize
+5. dotenv
+6. Insomnia
+
+## Link to Video Walkthrough:
+
+
+## Screenshots:
+![](/images/insomnia1.png)
+![](/images/insomnia2.png)
+![](/images/insomnia3.png)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,11 +4,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findAll({
+    const productData = await Product.findAll({
       include: [{ model: Category}, { model: Tag }],
     });
     res.status(200).json(productData);
@@ -18,11 +18,11 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findByPk(req.params.id, {
+    const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category}, { model: Tag }],
     });
 
@@ -38,7 +38,7 @@ router.get('/:id', (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -47,7 +47,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  await Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -70,9 +70,9 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
-  Product.update(req.body, {
+  await Product.update(req.body, {
     where: {
       id: req.params.id,
     },
@@ -111,10 +111,10 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = Product.destroy({
+    const productData = await Product.destroy({
       where: {
         id: req.params.id,
       },
